68736596edab9cf7d90ac3cd622ba37a69b04623
PratapPresolv360 <git config --global user.email>
2024-08-11 23:01:35 +0530

    folder structure changed

---
+++ electron/main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  // Load your React app
+  win.loadURL("http://localhost:3000");
+
+  // Open the DevTools (optional)
+  win.webContents.openDevTools();
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
